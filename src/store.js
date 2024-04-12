5cb42257d5fb81df2eb3c58bde6fb84c7fcf0b6b
ayushgadre <ayushgadre786@gmail.com0>
2024-04-12 09:28:31 +0530

    Minor Fixes

--- src/store.js
+++ src/store.js
@@ -1,8 +1,28 @@
-import { createStore, applyMiddleware } from "redux";
-import { rootReducer } from "./redux/rootReducer";
-import thunk from "redux-thunk";
+// import { createStore, applyMiddleware } from "redux";
+// import { rootReducer } from "./redux/rootReducer"; // Updated import for rootReducer
+// import thunk from "redux-thunk"; // Correct import for thunk
 
-// Create the Redux store using the rootReducer and apply middleware (thunk for async actions)
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// // Create the Redux store using the rootReducer and apply middleware (thunk for async actions)
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// export default store;
+// import { configureStore } from "@reduxjs/toolkit";
+// import rootReducer from "./reducers"; // Your combined reducers
+// import { rootReducer } from "./reducers";
+
+// const store = configureStore({
+//   reducer: rootReducer,
+//   // Other middleware and options if needed
+// });
+
+// export default store;
+
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers"; // Import rootReducer only once
+
+const store = configureStore({
+  reducer: rootReducer,
+  // Other middleware and options if needed
+});
 
 export default store;
